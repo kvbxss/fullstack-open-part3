49f3e2f3677503f066f21aa43f95f2fe08ed604c
kvbxss <kubadownar444@gmail.com>
2023-07-19 17:39:51 +0200

    step3.1

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,48 @@
-const express = require('express')
-  const app = express()
-  
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-  
-  
-  
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => {
-      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-      return note.id === id
-    })
-    console.log(note)
-    response.json(note)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const express = require('express');
+const app = express();
+
+// Define the phonebook entries array
+const phonebookEntries = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+];
+
+// Route handler for getting all phonebook entries
+app.get('/api/persons', (request, response) => {
+  response.json(phonebookEntries);
+});
+
+// Route handler for getting a specific phonebook entry by ID
+app.get('/api/persons/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  const entry = phonebookEntries.find(entry => entry.id === id);
+
+  if (!entry) {
+    response.status(404).json({ error: 'Entry not found' });
+  } else {
+    response.json(entry);
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
